e11d4f679eccf7ebf82b91b3c4208f74ea1a9e52
Phyoe <1997kophyoe@gmail.com>
2025-09-19 13:50:40 +0900

    feat: update configuration files to include license and build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'libs': ['crypto-js', 'exceljs', 'xlsx', '@vercel/speed-insights'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 3000,
+  }
 })
